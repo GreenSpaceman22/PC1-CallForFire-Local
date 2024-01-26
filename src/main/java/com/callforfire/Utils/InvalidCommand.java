82638fa689bc91e2822e301c1772a8bef0ec7104
CJish <christopheru82@gmail.com>
2024-01-26 15:48:32 -0800

    added InvalidCommand class, AB#94

---
+++ src/main/java/com/callforfire/Utils/InvalidCommand.java
@@ -0,0 +1,21 @@
+package com.callforfire.Utils;
+
+import com.apps.util.Console;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class InvalidCommand {
+
+    // method called when the game doesn't understand the input
+    public static void showInvalidCommand(List<String> parsedText) {
+        // TODO should probably add this to a JSON
+        // displays a general help text and returns
+        System.out.println("Sorry, I didn't understand that.\n");
+        Console.pause(200);
+        System.out.println("Try rephrasing your command.\n");
+        Console.pause(200);
+        System.out.println("Or you can type 'help' for the help menu, or 'quit' to quit the game.");
+        return;
+    }
+}
