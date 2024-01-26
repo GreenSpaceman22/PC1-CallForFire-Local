318801f13db6932ebe09aca39e6dd36e2de8c18a
SSPENELOPE <typoepping@gmail.com>
2024-01-26 13:09:01 -0500

    Stash

---
+++ src/main/java/com/callforfire/App/CallForFire_App.java
@@ -0,0 +1,21 @@
+package com.callforfire.App;
+
+//This is where all our of our game engines and logic will run from
+public class CallForFire_App {
+    private String currentLocation;
+
+    // TODO: Build and add the TextParser Here
+
+    // TODO: Build and add the OptionReader Here
+
+
+    // Methods
+    public void run() {
+        // Game logic to run the game goes in here
+    }
+
+    public void intialize() {
+        // TODO: Build the splash screen and credits in here, follow dev ops for more clear instruction
+    }
+
+}
