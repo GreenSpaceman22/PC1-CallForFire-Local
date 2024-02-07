052de3275a14497becd67305d2ebf6253ffabf36
SSPENELOPE <typoepping@gmail.com>
2024-02-07 11:29:44 -0500

    Added a copy of the locations to reset the locations json, AB#107

--- src/com/callforfire/GameEngines/SupportEngines/MessageReader.java
+++ src/com/callforfire/GameEngines/SupportEngines/MessageReader.java
@@ -1,6 +1,6 @@
 package com.callforfire.GameEngines.SupportEngines;
 
-import com.apps.util.Console;
+import com.callforfire.Models.Location;
 import com.callforfire.Models.NPC;
 
 import java.util.List;
@@ -21,17 +21,38 @@ public class MessageReader {
         System.out.println(npc.getName() + ": " + npc.getDialogue());
     }
 
+    public static void printDialogueError() {
+        System.out.println("Sorry we cannot find a character with that name, make sure you spell it the same way it is presented");
+    }
+
     // MOVING MESSAGES
     public static void printMoveError() {
         System.out.println("Sorry you can't move there");
     }
 
     public static void printLocationMessage(String locationDescription, String north, String south, String east, String west, String currentLocation) {
-        Console.clear();
-        System.out.println("Your Current Location: " + currentLocation);
+//        System.out.println("Your Current Location: " + currentLocation);
         System.out.println(locationDescription);
         System.out.println("To your north is " + north + ", to your east is " + east);
         System.out.println("To your south is " + south + ", to your west is " + west);
+    }
+
+    public static void printLocationMessage(Location location) {
+        System.out.println(location.getDescription());
+        System.out.println("To your north is " + location.getNorth() + ", to your east is " + location.getEast());
+        System.out.println("To your south is " + location.getSouth() + ", to your west is " + location.getWest());
+        if(location.getItems().size() >= 1) {
+            System.out.print("There is a ");
+            for(int i = 0; i < location.getItems().size(); i++) {
+                System.out.print(location.getItems().get(i));
+                if(i < location.getItems().size() - 1) {
+                    System.out.print(", ");
+                }
+            }
+            System.out.println(" here");
+        } else {
+            System.out.println("There is nothing useful here");
+        }
     }
 
     // ITEM MESSAGES
