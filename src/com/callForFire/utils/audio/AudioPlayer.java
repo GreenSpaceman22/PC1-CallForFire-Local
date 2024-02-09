afdab3fbc12fd5dfe0f90bfa7d06992a251d2553
GreenSpaceman22 <titus-kovalik@outlook.com>
2024-02-09 15:32:17 -0800

    constructed audio playing method in utils/audio and added audio files to Data/audio directory. AB#123

--- src/com/callForFire/utils/audio/AudioPlayer.java
+++ src/com/callForFire/utils/audio/AudioPlayer.java
@@ -1,15 +1,38 @@
 package com.callForFire.utils.audio;
 
 public class AudioPlayer {
-    public static void audioPlayer(){
-        try {
-            String audioCmd = "cmd /c start wmplayer Data/audio/javaProjAudi.wav";
 
-            Process execute = Runtime.getRuntime().exec(audioCmd);
-            execute.waitFor();
+    public static boolean isMuted = false;
+    public static Process execute;
 
-        } catch (Exception e) {
-            e.printStackTrace();
+    public static String audioCmd = "cmd /c start /B Data/audio/javaProjAudi.wav";
+
+    public static void audioPlayer() {
+        if (execute == null) {
+            try {
+                System.out.println("Opening audio");
+                String audioCmd = "cmd /c start /B Data/audio/javaProjAudi.wav";
+
+                if (!isMuted) {
+                    Thread thread = new Thread(() -> {
+                        try {
+                            while (!isMuted) {
+                                execute = Runtime.getRuntime().exec(audioCmd);
+                                execute.waitFor();
+                                Thread.sleep(91000);
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    });
+                    thread.start();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (execute != null) {
+            execute.destroy();
         }
     }
 }
